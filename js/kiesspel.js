55f3f18523471495b824d8c5cba06ab27bbb0a85
RooskenDaniel <daniel.roosken@student.nhlstenden.com>
2020-10-21 20:23:53 +0200

    index en folderindeling

---
+++ js/kiesspel.js
@@ -0,0 +1,6 @@
+var koffie = 0;
+var urlString = window.location.href;
+var url = new URL(urlString);
+koffie = url.searchParams.get('koffie');
+console.log(koffie);
+document.getElementById("memory").href = 'memory.html?koffie=' + koffie;
