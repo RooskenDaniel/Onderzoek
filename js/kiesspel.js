bd289437b64fcafc6e125f818673d2753cebe688
KoenSomsen <koen.somsen@student.nhlstenden.com>
2020-10-22 12:17:49 +0200

    Reactie test toegevoegd

--- js/kiesspel.js
+++ js/kiesspel.js
@@ -5,3 +5,4 @@ koffie = url.searchParams.get('koffie');
 console.log(koffie);
 document.getElementById("memory").href = 'memory.html?koffie=' + koffie;
 document.getElementById("reflectie").href = 'reflectie.html?koffie=' + koffie;
+document.getElementById("reactie").href = 'reactie.html?koffie=' + koffie;
